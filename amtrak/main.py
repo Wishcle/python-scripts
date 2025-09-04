ea5c2897ac95dca8e214562f44a9a35410e5a863
Wishcle <cpmqaq@gmail.com>
2025-09-03 20:49:49 -0400

    amtrak: Move script entrypoint to top-level

---
+++ amtrak/main.py
@@ -0,0 +1,9 @@
+
+from amtrak import search
+
+def main():
+    s = search.Searcher()
+    s.search()
+
+if __name__ == "__main__":
+    main()
---
+++ amtrak/src/amtrak/search.py
@@ -0,0 +1,8 @@
+
+class Searcher:
+
+    def __init__(self):
+        pass
+
+    def search(self) -> None:
+        print("searching...")
